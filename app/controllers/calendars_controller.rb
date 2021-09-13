adf2354cde25005eb7f95b5e9ead6533007e00bd
ryo1374 <rxyxomu419@gmail.com>
2021-09-13 09:47:06 +0900

    修正箇所2 完了

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -13,7 +13,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -32,7 +32,6 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-<<<<<<< Updated upstream
 
 
       wday_num = Date.today.wday[0]# wdayメソッドを用いて取得した数値
@@ -44,8 +43,7 @@ class CalendarsController < ApplicationController
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays:  wdays[x] }
 
 =======
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
->>>>>>> Stashed changes
+
       @week_days.push(days)
     end
 
